36f7f9774448ef9594dbd5ab627d3acc56555856
Bhavya Khurana <69009796+TheNewC0der-24@users.noreply.github.com>
2022-10-28 02:31:38 +0530

    Create the component for background animation

---
+++ src/BackgroundAnimate.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+function BackgroundAnimate() {
+    return (
+        <ul className="background">
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+        </ul>
+    )
+}
+
+export default BackgroundAnimate;
