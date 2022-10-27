ca34924b637ca27bcbb148780ac6c22a622803e2
Bhavya Khurana <69009796+TheNewC0der-24@users.noreply.github.com>
2022-10-28 02:32:03 +0530

    import the background animation component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import InputShortener from './InputShortener';
+import BackgroundAnimate from './BackgroundAnimate';
 
 function App() {
   return (
     <div className='container'>
       <InputShortener />
+      <BackgroundAnimate />
     </div>
   );
 }
