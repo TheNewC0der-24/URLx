02d59920571733cbd55f092fbbfdd5f2e542f457
Bhavya Khurana <69009796+TheNewC0der-24@users.noreply.github.com>
2022-10-28 03:09:47 +0530

    import result component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import InputShortener from './InputShortener';
 import BackgroundAnimate from './BackgroundAnimate';
+import Result from './Result';
 
 function App() {
   return (
     <div className='container'>
       <InputShortener />
       <BackgroundAnimate />
+      <Result />
     </div>
   );
 }
