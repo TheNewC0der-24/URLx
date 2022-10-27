2686025150bdc56d39297f892e62d6e295d592b7
Bhavya Khurana <69009796+TheNewC0der-24@users.noreply.github.com>
2022-10-28 03:09:18 +0530

    Create Result component

---
+++ src/Result.js
@@ -0,0 +1,12 @@
+
+const Result = () => {
+
+    return (
+        <div className="result">
+            <p>Result</p>
+            <button>Copy to Clipboard</button>
+        </div>
+    )
+}
+
+export default Result;
