49a88ef8fc0217be9fdd113181cb28be0a766261
Bhavya Khurana <bhavyakhurana002@gmail.com>
2022-11-13 03:02:07 +0530

    ✨ feat: add copy to clipboard functionality

--- src/Result.js
+++ src/Result.js
@@ -1,10 +1,29 @@
+import React, { useState, useEffect } from "react";
+
+import CopyToClipboard from 'react-copy-to-clipboard';
 
 const Result = () => {
 
+    const [shortenUrl, setShortenUrl] = useState("Hello World");
+    const [copied, setCopied] = useState(false);
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setCopied(false);
+        }, 1000);
+        return () => clearTimeout(timer);
+    }, [copied])
+
+
     return (
         <div className="result">
-            <p>Result</p>
-            <button>Copy to Clipboard</button>
+            <p>{shortenUrl}</p>
+            <CopyToClipboard
+                text={shortenUrl}
+                onCopy={() => setCopied(true)}
+            >
+                <button className={copied ? "copied" : ""}>Copy to Clipboard</button>
+            </CopyToClipboard>
         </div>
     )
 }
