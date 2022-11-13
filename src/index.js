57e204f267ffde4ac612aec16a7fd25806b18710
Bhavya Khurana <bhavyakhurana002@gmail.com>
2022-11-13 19:54:55 +0530

    import antd min.css

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
+import 'antd/dist/antd.min.css';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
