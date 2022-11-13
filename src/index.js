6334d190565cc774f26d3457da8179a3b6bbe8a4
Bhavya Khurana <bhavyakhurana002@gmail.com>
2022-11-13 20:04:07 +0530

    Remove antd min.css import

--- src/index.js
+++ src/index.js
@@ -4,8 +4,6 @@ import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
-import 'antd/dist/antd.min.css';
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
