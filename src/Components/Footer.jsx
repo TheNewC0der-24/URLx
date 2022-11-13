91df5d27a426e9a7ec56b82e8fa56ac678a6d235
Bhavya Khurana <bhavyakhurana002@gmail.com>
2022-11-14 01:11:10 +0530

    Create footer

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <>
+            <div className='footer'>
+                <h5>© 2022 URLx - Tool to shorten a long link.</h5>
+            </div>
+        </>
+    )
+}
+
+export default Footer;
