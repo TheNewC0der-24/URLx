14b039735397ed234a3ee0a5afe256ce1d586dc0
Bhavya Khurana <bhavyakhurana002@gmail.com>
2022-11-14 02:29:17 +0530

    register to service worker

--- src/index.js
+++ src/index.js
@@ -14,5 +14,5 @@ root.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.unregister();
+serviceWorkerRegistration.register();
 
